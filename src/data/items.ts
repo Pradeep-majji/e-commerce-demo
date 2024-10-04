eb65ef0d96b357568bb9c0075158ed64540ded96
Pradeep Kumar Majji <pradeep.majji@winfosolutions.com>
2024-10-04 16:15:16 +0530

    updated for photos

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,4 +1,7 @@
-
+import Shoe from '../assets/Shoe.jpg';
+import ProductB from '../assets/Product-B.jpg';
+import productC from '../assets/productc.webp';
+import productD from '../assets/productd.avif';
 
 export interface Item {
     id: number;
@@ -8,9 +11,9 @@ export interface Item {
   }
   
   export const items: Item[] = [
-    { id: 1, name: 'Product A', price: 19.99, image: 'Shoe.jpg' },
-    { id: 2, name: 'Product B', price: 29.99, image: 'Product-B.jpg' },
-    { id: 3, name: 'Product C', price: 9.99, image: 'productc.webp' },
-    { id: 4, name: 'Product D', price: 49.99, image: 'productd.avif' },
+    { id: 1, name: 'Product A', price: 19.99, image: Shoe },
+    { id: 2, name: 'Product B', price: 29.99, image: ProductB },
+    { id: 3, name: 'Product C', price: 9.99, image: productC },
+    { id: 4, name: 'Product D', price: 49.99, image: productD },
   ];
   
